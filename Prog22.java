93df6714879f08d82b0fa736dffdda1e829a351f
samrudshetty21 <samrudshetty21@gmail.com>
2025-12-02 14:16:03 +0530

    Create Prog22.java

---
+++ Prog22.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(10, 5);
+        Rectangle r2 = new Rectangle(7);
+
+        System.out.println("Area of Rectangle: " + r1.area());
+        System.out.println("Area of Square: " + r2.area());
+    }
+}
+
+class Rectangle {
+    int length, width;
+
+    Rectangle(int l, int w) {
+        length = l;
+        width = w;
+    }
+
+    Rectangle(int side) {
+        length = width = side;
+    }
+
+    int area() {
+        return length * width;
+    }
+}
