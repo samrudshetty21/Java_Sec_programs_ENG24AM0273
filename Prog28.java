7b1de1f69dbb18a42ec8e864cc09feee5aa074cd
samrudshetty21 <samrudshetty21@gmail.com>
2025-12-02 14:22:40 +0530

    Create Prog28.java

---
+++ Prog28.java
@@ -0,0 +1,15 @@
+class Counter {
+    static int count = 0;
+
+    Counter() {
+        count++;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        new Counter();
+        new Counter();
+        System.out.println(Counter.count);
+    }
+}
