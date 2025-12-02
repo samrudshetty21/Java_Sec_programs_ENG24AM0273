a53d3c3eb7fd15fe7f01669c05a1f1dcebf40a80
samrudshetty21 <samrudshetty21@gmail.com>
2025-12-02 14:24:16 +0530

    Create Prog39.java

---
+++ Prog39.java
@@ -0,0 +1,21 @@
+class Student {
+    String name;
+    int marks;
+
+    Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+}
+
+class Example {
+    static void updateMarks(Student s, int extra) {
+        s.marks += extra;
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Jit", 80);
+        updateMarks(s1, 10);
+        System.out.println(s1.name + ": " + s1.marks);
+    }
+}
